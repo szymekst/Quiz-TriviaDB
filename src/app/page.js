9289a0f49f2766db771c6fe34086dcdb1e320d83
szymekst <Denek222@gmail.com>
2025-01-12 02:16:59 +0100

    [Change] Categories UI

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import ButtonDefault from "@/components/ButtonDefault";
 
 export default function Home() {
     return (
-        <div className="flex flex-col flex-1 items-center pt-10 h-dvh">
+        <div className="container px-4 flex flex-col flex-1 items-center pt-10 h-dvh">
             <p className="mb-16 font-semibold text-2xl text-black">
                 Quizaz - Test your knowleade!
             </p>
             <CategorySection></CategorySection>
-            <div className="flex justify-center mt-auto w-full bg-white py-5">
+            <div className="flex justify-center w-full mt-5 pb-5">
                 <ButtonDefault text={"Start"} />
             </div>
         </div>
--- src/components/CategorySection.js
+++ src/components/CategorySection.js
@@ -15,22 +15,28 @@ const CategorySection = () => {
     }, []);
 
     return (
-        <div className="flex flex-col w-full items-center h-full">
+        <div className="container flex flex-col items-center h-full">
             <h1 className="mb-20 text-3xl text-blue font-semibold max-w-[690px]">
                 Select the category you are interested in...
             </h1>
-            <select
-                disabled={categories.length > 0 ? false : true}
-                className="w-[510px] text-center"
-            >
-                {categories.length > 0 ? (
-                    categories.map((cat, index) => (
-                        <option key={index}>{cat.name}</option>
-                    ))
-                ) : (
-                    <option>Loading...</option>
-                )}
-            </select>
+            <div className="flex flex-wrap gap-3">
+                {categories.length > 0
+                    ? categories.map((cat, index) => (
+                          <label
+                              key={index}
+                              className="bg-white flex items-center justify-center p-2 rounded-md cursor-pointer hover:bg-green-100 transition-all labels has-[:checked]:bg-green-100"
+                          >
+                              <p>{cat.name}</p>
+                              <input
+                                  type="radio"
+                                  name="categories"
+                                  defaultChecked={index === 0 ? true : false}
+                                  className="hidden"
+                              />
+                          </label>
+                      ))
+                    : ""}
+            </div>
         </div>
     );
 };
